7e6345859ed57630b8dafa78e8b76d63ad2145e2
Gilang Putera Mahardhika <gilangmahardhika@gmail.com>
2013-10-07 22:32:14 +0700

    remove converter.rb

--- lib/indonesian_currency_format/railtie.rb
+++ lib/indonesian_currency_format/railtie.rb
@@ -1,9 +1,9 @@
-require "indonesian_currency_format/converter"
+require "indonesian_currency_format/rupiah_converter"
 module IndonesianCurrencyFormat
   # include ActionView::Helpers::NumberHelper
   class Railtie < Rails::Railtie
-    initializer "indonesian_currency_format.convert" do
-      ActionView::Base.send :include, Converter
+    initializer "indonesian_currency_format.rupiah_converter" do
+      ActionView::Base.send(:include, RupiahConverter)
     end
   end
 end
